f9a0acc3f5a4fa39f0fcd650ce831a431b6844be
Ashish <ashish@drillmaps.com>
2021-12-03 11:32:59 -0800

    Checking in disjoint Sets

---
+++ TrieExample/src/edu/northeastern/ashish/Main.java
@@ -0,0 +1,19 @@
+package edu.northeastern.ashish;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Trie trie = new Trie();
+        trie.addString("ball");
+        trie.addString("bat");
+        trie.addString("best");
+        trie.addString("belt");
+        System.out.println();
+
+
+
+    }
+}
